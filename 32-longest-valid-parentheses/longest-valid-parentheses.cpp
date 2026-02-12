c98a291271895e25226e978039380367559c9e3a
Gagan Yadav <gaganyadav983911@gmail.com>
2026-02-12 18:22:43 +0530

    Time: 6 ms (13.62%) | Memory: 10.3 MB (90.94%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int count=0,max_valid=0,mult=0;
+        int count=0,max_valid=0,mult=0, count2=0,max_valid2=0,mult2=0,n=s.length();
         for(int i=0;i<s.length();i++){
             if(s[i]=='(') count++;
             else count--;
-            if(s[i]==')') mult++;
-            if(count<0){
-                mult=0;
-                count=0;
-            }
-            if(count==0) max_valid=max(max_valid,mult*2);
-        }
-        int count2=0,max_valid2=0,mult2=0;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s[i]==')') count2++;
+            if(s[n-i-1]==')') count2++;
             else count2--;
-            if(s[i]=='(') mult2++;
+            if(s[i]==')') mult++;
+            if(s[n-i-1]=='(') mult2++;
             if(count2<0){
                 mult2=0;
                 count2=0;
             }
             if(count2==0) max_valid2=max(max_valid2,mult2*2);
+            if(count<0){
+                mult=0;
+                count=0;
+            }
+            if(count==0) max_valid=max(max_valid,mult*2);
         }
         return max(max_valid,max_valid2);
     }
