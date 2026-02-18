faf2445f5f1d8c27f9bf4ecbbf624d865b5be1c2
Gagan Yadav <gaganyadav983911@gmail.com>
2026-02-18 19:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (73.54%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -7,7 +7,7 @@ public:
         }
         for(int i=0;i<nums.size();i++){
             if(used[i]) continue;
-            if(i<nums.size()-1 &&nums[i]==nums[i+1]&& !used[i+1]){
+            if(i>0 &&nums[i]==nums[i-1]&& !used[i-1]){
                 continue;
             }
             used[i]=true;
