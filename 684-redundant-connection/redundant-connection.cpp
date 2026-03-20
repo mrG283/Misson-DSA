b913a336a7fa2d2a55f4e218f42f15dc337b853f
Gagan Yadav <gaganyadav983911@gmail.com>
2026-03-20 19:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (40.30%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,35 +1,56 @@
+class DSU{
+public:
+    vector<int>parent;
+    vector<int>rank;
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+    int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+    void Union(int x,int y){
+        int x_p=find(x);
+        int y_p=find(y);
+        if(x_p==y_p){
+            return;
+
+        }
+        else if(rank[x_p]>rank[y_p]){
+            parent[y_p]=x_p;
+        }
+        else if(rank[y_p]>rank[x_p]){
+            parent[x_p]=y_p;
+        }
+        else{
+            parent[x_p]=y_p;
+            rank[y_p]++;
+        }
+    }
+};
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>>&adj,int u,int v,vector<bool>&visited){
-        visited[u]=true;
-        if(u==v){
-            return true;
-        }
-        for(int a:adj[u]){
-            if(visited[a]){
-                continue;
 
-            }
-            if(dfs(adj,a,v,visited)){
-                return true;
-            }
-        }
-        return false;
         
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        unordered_map<int,vector<int>>adj;
-        for (int i=0;i<n;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            vector<bool>visited(n,false);
-            if(adj.find(u)!=adj.end()&&adj.find(v)!=adj.end()&&dfs(adj,u,v,visited)){
-                return edges[i];
+        DSU dsu(n+1);
+        for(auto edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            if(dsu.find(u)==dsu.find(v)){
+                return edge;
             }
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            dsu.Union(u,v);
         }
+    
         return {};
         
     }
