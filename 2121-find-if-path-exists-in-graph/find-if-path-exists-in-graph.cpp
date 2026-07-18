da616b490b9711457b8d9de50f6352efd1cf5d1e
Gagan Yadav <gaganyadav983911@gmail.com>
2026-07-18 16:35:59 +0530

    Time: 403 ms (34.99%) | Memory: 311.9 MB (38.56%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& edges, int source, int destination,vector<vector<int>>&adj,vector<bool>&visited){
-        if(source==destination){
-            return true;
-        }
-        visited[source]=true;
-        for(auto n:adj[source]){
-            if(!visited[n]){
-                if(dfs(edges,n,destination,adj,visited)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    // bool dfs(vector<vector<int>>& edges, int source, int destination,vector<vector<int>>&adj,vector<bool>&visited){
+    //     if(source==destination){
+    //         return true;
+    //     }
+    //     visited[source]=true;
+    //     for(auto n:adj[source]){
+    //         if(!visited[n]){
+    //             if(dfs(edges,n,destination,adj,visited)){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>>adj(n);
         for(auto e:edges){
@@ -21,6 +21,23 @@ public:
             adj[e[1]].push_back(e[0]);
         }
         vector<bool>visited(n,false);
-        return dfs(edges,source,destination,adj,visited);
+        // return dfs(edges,source,destination,adj,visited);
+        queue<int>q;
+        q.push(source);
+        visited[source]=true;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if(node==destination){
+                return true;
+            }
+            for(auto n:adj[node]){
+                if(!visited[n]){
+                    q.push(n);
+                    visited[n]=true;
+                }
+            }
+        }
+        return false;
     }
 };
